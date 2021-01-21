ac1e764da8f5c8fdc0d7402ace597e987a05d4cd
jglez <jglez.cs@gmail.com>
2021-01-21 00:45:51 -0600

    Created ACTIONS.js, charReducer.js, and Character.js component

---
+++ src/reducers/charReducer.js
@@ -0,0 +1,15 @@
+// Initial State Values
+const initialState = {
+  name: '',
+  image: '',
+  isFetching: false,
+  error: ''
+}
+
+// Pass initial state values into reducer
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
